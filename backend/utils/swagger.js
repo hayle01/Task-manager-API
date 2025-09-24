a7e90ed14f911f597a6d8979f2ea337eb97df79e
hayle01 <mabdirahim832@gmail.com>
2025-09-24 16:01:25 +0300

    Issue solved

--- backend/utils/swagger.js
+++ backend/utils/swagger.js
@@ -1,45 +1,42 @@
 import swaggerJSDoc from "swagger-jsdoc";
-import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-
-dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Task Manager API",
-      version: "1.0.0",
-      description: "API documentation for our task manager backend",
-    },
-    servers: [
-      {
-        url:
-          process.env.NODE_ENV === "development"
-            ? "http://localhost:3000"
-            : "https://task-manager-api-99tg.onrender.com",
+export let swaggerSpec;
+
+if (process.env.NODE_ENV === "development") {
+  const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Task Manager API",
+        version: "1.0.0",
+        description: "API documentation for the task manager backend",
       },
-    ],
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: "http",
-          scheme: "bearer",
-          bearerFormat: "JWT",
+      servers: [
+        {
+          url: "http://localhost:3000",
+        },
+      ],
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: "http",
+            scheme: "bearer",
+            bearerFormat: "JWT",
+          },
         },
       },
+      security: [{ bearerAuth: [] }],
     },
-    security: [{ bearerAuth: [] }],
-  },
-  apis: [
-    path.join(__dirname, "../routes/auth.js"),
-    path.join(__dirname, "../routes/tasks.js"),
-]
+    apis: [
+      path.join(__dirname, "../routes/auth.js"),
+      path.join(__dirname, "../routes/tasks.js"),
+    ],
+  };
 
-};
-
-export const swaggerSpec = swaggerJSDoc(options);
+  swaggerSpec = swaggerJSDoc(options);
+}
