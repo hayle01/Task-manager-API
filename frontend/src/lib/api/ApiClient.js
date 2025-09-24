f32b1d39e55e9f22e7ce9237f905149bd1efd4a7
hayle01 <mabdirahim832@gmail.com>
2025-09-24 15:20:02 +0300

    production URL

--- frontend/src/lib/api/ApiClient.js
+++ frontend/src/lib/api/ApiClient.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 import useAuthStore from "../Store/authStore";
 
-const API_URL = 'http://localhost:3000/api'
+// const API_URL = 'http://localhost:3000/api'
+const API_URL = 'https://task-manager-api-8byk.onrender.com/api'
 
 const api = axios.create({
     baseURL: API_URL,
